3aa6efde337338d676cb38c4d17085fd1f13d2b5
jaymarf <jaymarf@microsoft.com>
2016-02-25 19:56:05 -0800

    Update depro.sh

--- Artifacts/linux-AzureDeProvision/depro.sh
+++ Artifacts/linux-AzureDeProvision/depro.sh
@@ -7,6 +7,6 @@ set -e
 sleep 10 ; exit 
 #shutdown -h +1
 #delay executing this command until agent reports
-#sleep 10 && waagent -force -deprovision+user 
+sleep 10; waagent -force -deprovision+user 
 
  
